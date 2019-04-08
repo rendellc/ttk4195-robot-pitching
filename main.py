863b20aa903a2b7836fd890464bbf0024cf21706
taklab <taklab>
2019-04-08 20:43:30 +0200

    worse results after 200000000 training runs

--- main.py
+++ main.py
@@ -13,29 +13,47 @@ def controller_template(parameters):
     q1dot = lambda t: 3*p[3]*t**2 + 2*p[2]*t + p[1]
 
     # Use the two last parameters as controller gains
-    controller_func = lambda t, q: p[~1]*(q1(t) - q[0]) + p[~0]*(q1dot(t) - q[2])
+    #controller_func = lambda t, q: p[~1]*(q1(t) - q[0]) + p[~0]*(q1dot(t) - q[2])
+    controller_func = lambda t, q: 8*(q1(t) - q[0]) + 24*(q1dot(t) - q[2])
 
     return controller_func
 
+def load_results(filename):
+    with open(filename, 'r') as infile:
+        data = json.load(infile)
+        best_reward = data.get('reward', 0.0)
+        best_parameters = data.get('parameters', [0,0,0,0,0,0])
+        
+        return best_reward, best_parameters
+    
+def save_results(filename, reward, parameters):
+    data = dict()
+    data['reward'] = reward
+    data['parameters'] = parameters
+    with open(filename,'w') as outfile:
+        json.dump(data, outfile)
+
+def create_limits(parameters, radius):
+    limits = []
+    for p in parameters:
+        limits.append((p - radius, p + radius))
+    
+    return limits
+
 def main():
     env = environment.Environment()
-
-    best_reward = 0.0
-    best_parameters = [0,0,0,0,0]
-    limits = [(-10,10), (-10,10), (-10,10), (0,10), (0,10)]
-    with open('results.json', 'r') as infile:
-        data = json.load(infile)
-        best_reward = data.get('reward', best_reward)
-        best_parameters = data.get('parameters', best_parameters)
-        p = best_parameters
-        for i in range(len(limits)):
-            limits[i] = (p[i] - 10, p[i] + 10)
+    
+    best_reward, best_parameters = load_results('results.json')
+    limits = create_limits(best_parameters, 20)
 
     n_episodes = 100000
     n_failed = 0
-    for i in range(n_episodes):
-        if i%1000 == 0:
-            print(i, n_failed)
+    i = 0
+    while True:
+        i += 1
+        if i%10000 == 0:
+            print(i, n_failed/10000)
+            n_failed = 0
 
         controller_func, parameters = env.action_sample(controller_template, limits)
         reward, info = env.episode(controller_func)
@@ -46,17 +64,20 @@ def main():
         if reward > best_reward:
             best_reward = reward
             best_parameters = parameters
+            
+            file_best, file_parameters = load_results('results.json')
+            if file_best > best_reward:
+                # Use file parameters instead and keep searching
+                best_reward = file_best
+                best_parameters = file_parameters
+            else:
+                # Update file since we have found better parameters
+                save_results('results.json', best_reward, best_parameters)
 
             # move limits to be centered on best_parameters
-            for i in range(len(limits)):
-                limits[i] = (p[i] - 10, p[i] + 10)
+            limits = create_limits(best_parameters, 20)
 
-            print("best reward {0}, new limits {1}".format(reward, limits))
-            best = dict()
-            best['reward'] = best_reward
-            best['parameters'] = best_parameters
-            with open('results.json','w') as outfile:
-                json.dump(best, outfile)
+            print("best reward {0}, new limits {1}".format(best_reward, limits))
 
     # Replay best parameter setup with plotting
     env.sim.record_path = True
